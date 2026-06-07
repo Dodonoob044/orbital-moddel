d0e1aad4360e93ed8a3b1dc529181a964502c072
Dodonoob044 <rickdeschepper1511@gmail.com>
2026-06-07 13:00:24 +0200

    redirected the functions to func file
    redirected the functions to func file and improved the move  functions

---
+++ orbital model/func.py
@@ -0,0 +1,81 @@
+import data
+import math
+import pygame
+
+
+
+
+def object_size(max, min, scale):
+    scale_variable_min = 10e8/scale
+    size = max*scale_variable_min
+    if size<min:
+        size = min
+    return(size)
+
+def calculate_tracers(a, T, scale_variable_min):
+    tracers = (2*math.pi*a)/(10216870687/scale_variable_min)
+    trace_time = T/tracers
+    if trace_time<1:
+        trace_time = 1
+    return(trace_time)
+
+def solve_E_for_M(M, e):
+    E = M
+    while True:
+        correction = (E - e * math.sin(E) - M) / (1 - e * math.cos(E))
+        E = E - correction
+        if abs(correction) < 1e-6:
+            break
+    return E
+
+def solve_angle_and_range(t, e, a, n):
+    M = n*t
+    E = solve_E_for_M(M, e)
+    x = math.sqrt((1+e)/(1-e))*math.tan(E/2)
+    θ = 2*math.atan(x)
+    if θ< 0:
+        θ += (math.pi*2)
+    # now i have the angle i just need the distance
+    r = a*(1-e*math.cos(E))
+    return(θ, r)
+
+def draw_object_in_orbit(SCREEN, t, e, a, n, delay, color, size, important, p_adjust, scale, X_move_adjusted, Y_move_adjusted):
+    object_data = solve_angle_and_range((t+delay), e, a, n)
+    X_object = (math.cos(2*math.pi-object_data[0]+p_adjust) * object_data[1])/ scale
+    Y_object = (math.sin(2*math.pi-object_data[0]+p_adjust) * object_data[1])/ scale
+    
+    #adding potential movement
+    X = data.X_adjust + X_move_adjusted
+    Y = data.Y_adjust + Y_move_adjusted
+    #if important:
+    #    pygame.draw.circle(SCREEN, (0, 0, 0), ((X_object + X_adjust), (Y_object + Y_adjust)), size+3)
+    pygame.draw.circle(SCREEN, (color), ((X_object + X), (Y_object + Y)), size)
+
+
+
+def plannet(SCREEN, scale, t, X_move_adjusted, Y_move_adjusted, size_max, size_min, a, T, e, n, P_adjust, color, delay):
+    scale_variable_min = 10e8/scale
+
+    size = object_size(size_max, size_min, scale)
+    trace_time = calculate_tracers(a, T, scale_variable_min)
+    
+    for i in range (0, int(T), int(trace_time)):        
+        
+        l_data = solve_angle_and_range((i), e, a, n)
+
+        X_object = (math.cos(2*math.pi-l_data[0]+P_adjust) * l_data[1])/ scale
+        Y_object = (math.sin(2*math.pi-l_data[0]+P_adjust) * l_data[1])/ scale
+        X = data.X_adjust + X_move_adjusted
+        Y = data.Y_adjust + Y_move_adjusted
+
+        pygame.draw.circle(SCREEN, (data.color_trace), ((X_object + X), (Y_object + Y)), 1)
+    
+    l_data = solve_angle_and_range((t+delay), e, a, n)
+    
+    X_object = (math.cos(2*math.pi-l_data[0]+P_adjust) * l_data[1])/ scale
+    Y_object = (math.sin(2*math.pi-l_data[0]+P_adjust) * l_data[1])/ scale
+    
+    X = data.X_adjust + X_move_adjusted
+    Y = data.Y_adjust + Y_move_adjusted
+
+    pygame.draw.circle(SCREEN, (color), ((X_object + X), (Y_object + Y)), size)
---
+++ orbital model/main.py
@@ -0,0 +1,154 @@
+#agenda
+#calculate the correct periapsis adjustments
+#in seperate file make a better code for intercepts
+# make the moon
+# make moons, i dont know how many i expect. not a priority
+# make the camera move 
+
+#bugs
+
+
+#imports
+import pygame
+import time
+import math
+
+#related files
+import data
+import func
+
+pygame.init()
+
+#window details
+SCREEN_WITH = data.SCREEN_with
+SCREEN_HEIGHT = data.SCREEN_height
+SCREEN = pygame.display.set_mode((SCREEN_WITH, SCREEN_HEIGHT))
+
+
+t = 0
+scale = 10e8
+X_move = 0
+Y_move = 0
+scale_variable_min = 10e8/scale
+
+
+
+
+
+
+
+
+
+
+
+run =  True
+while run == True:
+    X_move_adjusted = X_move/scale
+    Y_move_adjusted = Y_move/scale
+    scale_variable_min = 1 * 1e9/scale
+    events = pygame.event.get()
+    SCREEN.fill((0, 0, 0))
+    time.sleep(0.01)
+    #if t<152:
+    t += 0.5
+    #t = 152
+
+    #inner plannets
+    if scale <= 4.5e9:
+        #object scaling
+        
+        
+        
+        #Mars
+        func.plannet(SCREEN, scale, t, X_move_adjusted, Y_move_adjusted, data.size_Mars, data.size_Mars_min, data.Semimajor_axis_Mars, 
+        data.orbital_period_Mars, data.eccentricity_Mars, data.mean_motion_Mars, data.Mars_periapsis_adjustment, data.color_Mars, data.delay_Mars)
+        
+        #Earth
+        func.plannet(SCREEN, scale, t, X_move_adjusted, Y_move_adjusted, data.size_Earth, data.size_Earth_min, data.Semimajor_axis_Earth, 
+        data.orbital_period_Earth, data.eccentricity_Earth, data.mean_motion_Earth, data.Earth_periapsis_adjustment, data.color_Earth, data.delay_Earth)
+
+        #Venus
+        func.plannet(SCREEN, scale, t, X_move_adjusted, Y_move_adjusted, data.size_Venus, data.size_Venus_min, data.Semimajor_axis_Venus, 
+        data.orbital_period_Venus, data.eccentricity_Venus, data.mean_motion_Venus, data.Venus_periapsis_adjustment, data.color_Venus, data.delay_Venus)
+
+        #Mercury
+        func.plannet(SCREEN, scale, t, X_move_adjusted, Y_move_adjusted, data.size_Mercury, data.size_Mercury_min, data.Semimajor_axis_Mercury, 
+        data.orbital_period_Mercury, data.eccentricity_Mercury, data.mean_motion_Mercury, data.Mercury_periapsis_adjustment, data.color_Mercury, data.delay_Mercury)
+
+
+
+    #inner blur
+    else:
+        size_inner = data.Apoapsis_Mars/scale*2
+        line_with_inner = size_inner*0.386
+        inner_plannets = pygame.Rect((data.X_adjust-0.5*size_inner+X_move_adjusted), (data.Y_adjust-0.5*size_inner+Y_move_adjusted), size_inner, size_inner)
+        pygame.draw.ellipse(SCREEN, (data.color_inner_blur), inner_plannets, int(line_with_inner))
+    
+    #outer plannets
+
+    #Jupiter
+    func.plannet(SCREEN, scale, t, X_move_adjusted, Y_move_adjusted, data.size_Jupiter, data.size_Jupiter_min, data.Semimajor_axis_Jupiter, 
+    data.orbital_period_Jupiter, data.eccentricity_Jupiter, data.mean_motion_Jupiter, data.Jupiter_periapsis_adjustment, data.color_Jupiter, data.delay_Jupiter)
+    #Saturn
+    func.plannet(SCREEN, scale, t, X_move_adjusted, Y_move_adjusted, data.size_Saturn, data.size_Saturn_min, data.Semimajor_axis_Saturn, 
+    data.orbital_period_Saturn, data.eccentricity_Saturn, data.mean_motion_Saturn, data.Saturn_periapsis_adjustment, data.color_Saturn, data.delay_Saturn)
+    #Uranus
+    func.plannet(SCREEN, scale, t, X_move_adjusted, Y_move_adjusted, data.size_Uranus, data.size_Uranus_min, data.Semimajor_axis_Uranus, 
+    data.orbital_period_Uranus, data.eccentricity_Uranus, data.mean_motion_Uranus, data.Uranus_periapsis_adjustment, data.color_Uranus, data.delay_Uranus)
+    #Neptune
+    func.plannet(SCREEN, scale, t, X_move_adjusted, Y_move_adjusted, data.size_Neptune, data.size_Neptune_min, data.Semimajor_axis_Neptune, 
+    data.orbital_period_Neptune, data.eccentricity_Neptune, data.mean_motion_Neptune, data.Neptune_periapsis_adjustment, data.color_Neptune, data.delay_Neptune)
+
+
+
+    
+    
+    #Sol
+    
+    size_Sol_adjusted = func.object_size(data.size_Sol, data.size_Sol_min, scale)
+    if size_Sol_adjusted<5:
+        size_Sol_adjusted = 5
+    pygame.draw.circle(SCREEN, (255, 214, 74), (data.X_adjust+X_move_adjusted, data.Y_adjust+Y_move_adjusted), size_Sol_adjusted)
+
+
+
+    #quit function
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+        #zooming function
+        if scale/(1e8) >= 4:
+            if event.type == pygame.MOUSEWHEEL:
+                if scale/(1e9) <=4.5:
+                    scale -= 100000000*event.y
+                if scale/(1e9) > 4.5:
+                    scale -= 500000000*event.y            
+        if scale/(1e8) <= 4:
+            scale = 4e8
+        if scale/(1e8) > 350:
+            scale = 350e8
+        print(scale/1e8)
+
+    #moving function
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w] == True:
+        if key[pygame.K_d] == True:
+            X_move -= 1 * scale
+        if key[pygame.K_a] == True:
+            X_move += 1 * scale
+        Y_move += 1 * scale
+    elif key[pygame.K_s] == True:
+        if key[pygame.K_d] == True:
+            X_move -= 1 * scale
+        if key[pygame.K_a] == True:
+            X_move += 1 * scale
+        Y_move -= 1 * scale
+    elif key[pygame.K_d] == True:
+        X_move -= 1 * scale
+    elif key[pygame.K_a] == True:
+        X_move += 1 * scale
+            
+    pygame.display.update()
+
+pygame.quit()
